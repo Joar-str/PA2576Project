892caeddabe919025507e5aa70b76bdb2633bf8f
Joar-Str <joar.s.strandberg@hotmail.com>
2022-02-22 14:56:51 +0100

    Skapad "User" klass med createUser funktion där man skapar en user

--- Controller.py
+++ Controller.py
@@ -2,14 +2,28 @@ from kivymd.app import MDApp
 from kivy.uix.screenmanager import ScreenManager
 from kivy.lang import Builder
 from kivy.properties import ObjectProperty, ListProperty
+from main import User
 from os import path
 from inspect import currentframe, getfile
 
 class mainApp(MDApp):
-    scr3 = ObjectProperty()
+    created_password = ObjectProperty(None)
+    created_name = ObjectProperty(None)
+    PhoneNr = ObjectProperty(None)
+
     def build(self):
         self.sm = ScreenManager()
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "BlueGray"
+        self.sm.add_widget(Builder.load_file('first_page.kv'))
         self.sm.add_widget(Builder.load_file('login_page.kv'))
+        self.sm.add_widget(Builder.load_file('createAccount_page.kv'))
+
         return self.sm
+
+    def submit_btn(self):
+        User(self.sm.created_name.text, self.sm.created_password.text, self.sm.PhoneNr.text)
+        User.createUser()
+
 
 mainApp().run()
--- main.py
+++ main.py
@@ -19,33 +19,26 @@ connection = mysql.connect(user=MYSQL_USER,
 
 cnx = connection.cursor(dictionary=True)
 
-class mainApp(MDApp):
-    def build(self):
-        self.sm = ScreenManager()
-        self.sm.add_widget(Builder.load_file('login_page.kv'))
-        return self.sm
-
 
 class User:
-    def __init__(self):
-        pass
+
+    def __init__(self, email, password, phoneNr):
+        self.email = email
+        self.password = password
+        self.phoneNr = phoneNr
 
     def createUser(self):
-        self.email = str(input('email:'))
-        self.password = str(input('password:'))
-        self.phoneNr = int(input('phonenr:'))
         try:
-            cnx.execute(f"INSERT INTO User(email, password, phoneNr) Values('{self.email}', '{self.password}', '{self.phoneNr}')")
+            cnx.execute(f"INSERT INTO User(email, password, phoneNr) Values('{self.email}',"
+                        f" '{self.password}', '{self.phoneNr}')")
             connection.commit()
         except:
             print('fel.')
             connection.close()
 
 
+
+
 if __name__ == "__main__":
     pass
-    #t1 = User()
-    #t1.createUser()
 
-#cnx.execute('''INSERT Into User(email, password, phoneNr) values ('test1@..','123',12345)''')
-#cnx.execute('''SELECT * FROM User''')
