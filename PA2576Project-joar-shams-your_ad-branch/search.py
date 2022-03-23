73374d8b44ab5d89f2366098b6e9247ef46e4b86
Jakob <82160552+JakobJoppe@users.noreply.github.com>
2022-03-23 09:46:43 +0100

    Add files via upload

---
+++ PA2576Project-joar-shams-your_ad-branch/search.py
@@ -0,0 +1,20 @@
+from kivymd.uix.dialog import MDInputDialog, MDDialog
+from kivy.uix.textinput import TextInput
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import ObjectProperty
+
+
+class SearchPopupMenu(BoxLayout):
+    def __init__(self):
+        super().__init__()
+
+
+
+
+
+
+
+
+
+
+
